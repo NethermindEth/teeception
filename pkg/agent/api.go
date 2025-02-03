fcd90af667612dffa2d7db230073a597893c96f0
Franco Barpp Gomes <franco@nethermind.io>
2025-02-03 13:58:06 +0900

    fix: Fix error stringifying in deployment status response

--- pkg/agent/api.go
+++ pkg/agent/api.go
@@ -3,6 +3,7 @@ package agent
 import (
 	"context"
 	"encoding/base64"
+	"fmt"
 	"log/slog"
 	"net/http"
 
@@ -36,7 +37,7 @@ func (a *Agent) StartServer(ctx context.Context) error {
 	router.GET("/deployment", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"already_deployed":   a.accountDeploymentState.AlreadyDeployed,
-			"deployment_error":   a.accountDeploymentState.DeploymentErr.Error(),
+			"deployment_error":   fmt.Sprintf("%v", a.accountDeploymentState.DeploymentErr),
 			"deployed_at":        a.accountDeploymentState.DeployedAt,
 			"balance":            a.accountDeploymentState.Balance.String(),
 			"balance_updated_at": a.accountDeploymentState.BalanceUpdatedAt,
