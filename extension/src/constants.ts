0f47ca7aab08b6960ac1ebca60dc926e50cc1db6
Jorik Schellekens <joriksch@gmail.com>
2025-02-03 20:51:21 -0800

    Improve tooltips

--- extension/src/constants.ts
+++ extension/src/constants.ts
@@ -1,5 +1,5 @@
-export const AGENT_NAME_TOOLTIP_CONTENT = 'Name of your agent (max 31 characters)'
+export const AGENT_NAME_TOOLTIP_CONTENT = 'Name of your agent'
 export const SELECT_TOKEN_TOOLTIP_CONTENT = 'Select token for fees'
 export const SYSTEM_PROMPT_TOOLTIP_CONTENT =
-  "The System Prompt is your agent's foundation. Make it 'strong' to defend against attacks and ensure it stays on purpose."
-export const END_TIME_TOOLTIP_CONTENT = "When the agent will stop accepting new prompts";
+  "The System Prompt dictating your agent's behaviour. Make it 'strong' to defend against attacks and ensure it stays on purpose."
+export const END_TIME_TOOLTIP_CONTENT = "The time at which the agent will stop accepting challanges";
