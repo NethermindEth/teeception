50ff42d71853d66a1dd1aa7af36120e702b3b354
Jorik Schellekens <joriksch@gmail.com>
2025-02-06 00:40:30 -0800

    Lint unused vars

--- extension/src/content-script/components/modals/ConfirmationModal.tsx
+++ extension/src/content-script/components/modals/ConfirmationModal.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import { Button } from '@/components/ui/button'
 import { Dialog } from './Dialog'
 import { cn } from '@/lib/utils'
 import { MessageCircle } from 'lucide-react'
-import { debug } from '../../utils/debug'
 
 interface ConfirmationModalProps {
   open: boolean
