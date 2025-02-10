bb1156fd2cea701c2a8ea297068f6071f56665d7
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-02-10 13:22:49 +0530

    feat: Agent chat style changes

--- frontend/components/AgentChat.tsx
+++ frontend/components/AgentChat.tsx
@@ -7,22 +7,21 @@ import { Prompt } from '@/types'
 export const AgentChat = ({ prompts }: { prompts: Prompt[] }) => {
   return (
     <div>
-      <div className="flex flex-col mx-auto px-20 overflow-hidden whitespace-nowrap">
+      <div className="flex flex-col mx-auto px-20 overflow-hidden">
         <div className="animate-marquee inline-flex gap-4">
           {prompts.map((prompt, index) => (
             <div
-              className="flex items-start gap-3 w-full mb-4 p-8 rounded-lg bg-[#27313666] hover:bg-[#273136cc] transition-colors"
+              className="flex items-start gap-3 w-full mb-4 p-8 rounded-lg bg-[#27313666] hover:bg-[#273136cc] transition-colors max-w-[500px]"
               key={index}
             >
-              <div className="text-xs w-full min-w-[300px]">
+              <div className="text-sm w-full min-w-[300px]">
                 <p className="font-medium mb-1 flex items-center gap-2">
                   Attacker user{' '}
                   {prompt.is_success && (
                     <Image src={'/icons/crown.png'} width={16} height={16} alt="crown" />
                   )}
                 </p>
-                <p className="mb-1 text-[#D3E7F0] break-all max-w-[80%]">{prompt.prompt}</p>
-                <div className="flex justify-between"></div>
+                <p className="mb-1 text-[#D3E7F0] break-words">{prompt.prompt}</p>
               </div>
             </div>
           ))}
