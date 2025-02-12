26c3d2181d05b239807e205cafa133b5f64a6719
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-02-12 13:01:32 +0530

    feat: Add wallets

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,44 +1,56 @@
 'use client'
 
-import { StarknetConfig, publicProvider } from '@starknet-react/core'
-import { ControllerConnector } from '@cartridge/connector'
-import { sepolia } from '@starknet-react/chains'
-import { ACTIVE_NETWORK, STARKNET_CONFIG } from '@/constants'
+'use client'
+import React from 'react'
+import {
+  StarknetConfig,
+  publicProvider,
+  voyager,
+  argent,
+  braavos,
+  useInjectedConnectors,
+} from '@starknet-react/core'
+import { ACTIVE_NETWORK } from '@/constants'
 import { AddFundsProvider } from '@/contexts/AddFundsContext'
 
-const policies = {
-  contracts: {
-    [ACTIVE_NETWORK.agentRegistryAddress]: {
-      name: 'Agent Registry',
-      description: 'Allows interaction with the Agent Registry contract',
-      methods: [
-        {
-          name: 'Register Agent',
-          description: 'Register a new AI agent',
-          entrypoint: 'register_agent',
-        },
-        {
-          name: 'Transfer Agent',
-          description: 'Transfer ownership of an agent',
-          entrypoint: 'transfer',
-        },
-      ],
-    },
-  },
-}
+// const policies = {
+//   contracts: {
+//     [ACTIVE_NETWORK.agentRegistryAddress]: {
+//       name: 'Agent Registry',
+//       description: 'Allows interaction with the Agent Registry contract',
+//       methods: [
+//         {
+//           name: 'Register Agent',
+//           description: 'Register a new AI agent',
+//           entrypoint: 'register_agent',
+//         },
+//         {
+//           name: 'Transfer Agent',
+//           description: 'Transfer ownership of an agent',
+//           entrypoint: 'transfer',
+//         },
+//       ],
+//     },
+//   },
+// }
 
-const cartridgeConnector = new ControllerConnector({
-  policies,
-  defaultChainId: ACTIVE_NETWORK.chainId,
-  chains: [{ rpcUrl: ACTIVE_NETWORK.rpc }, { rpcUrl: STARKNET_CONFIG.mainnet.rpc }],
-})
+// const cartridgeConnector = new ControllerConnector({
+//   policies,
+//   defaultChainId: ACTIVE_NETWORK.chainId,
+//   chains: [{ rpcUrl: ACTIVE_NETWORK.rpc }, { rpcUrl: STARKNET_CONFIG.mainnet.rpc }],
+// })
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const { connectors } = useInjectedConnectors({
+    recommended: [argent(), braavos()],
+    includeRecommended: 'onlyIfNoConnectors',
+  })
   return (
     <StarknetConfig
-      chains={[sepolia]}
+      chains={[ACTIVE_NETWORK.chain]}
       provider={publicProvider()}
-      connectors={[cartridgeConnector]}
+      connectors={connectors}
+      explorer={voyager}
     >
       <AddFundsProvider>{children}</AddFundsProvider>
     </StarknetConfig>
