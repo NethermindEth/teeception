07fd054e97a8f8d900739577c5bf4290854055a5
Franco Barpp Gomes <franco@nethermind.io>
2025-02-21 11:09:59 +0900

    feat: Use DM Sans in header menu items

--- frontend/components/MenuItems.tsx
+++ frontend/components/MenuItems.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const MenuItems = () => {
   return (
-    <ul className="text-sm flex items-center justify-end md:justify-center">
+    <ul className="text-sm flex items-center justify-end md:justify-center header-menu-items">
       <li className="px-6">
         <Link href="/leaderboard" className="hover:text-white">
           Leaderboard
