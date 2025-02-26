3054392efab530ee1ee74ee069a428b841d6aa98
Franco Barpp Gomes <franco@nethermind.io>
2025-02-27 00:38:50 +0900

    feat: Set default RPC URL to free-rpc.nethermind.io

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,6 +1,6 @@
 import { constants } from 'starknet'
 import { mainnet, sepolia } from '@starknet-react/chains'
-export const DEFAULT_RPC_URL = 'https://api.cartridge.gg/x/starknet/sepolia'
+export const DEFAULT_RPC_URL = 'https://free-rpc.nethermind.io/sepolia-juno/v0_7'
 export const RPC_NODE_URL = process.env.NEXT_PUBLIC_RPC_NODE_URL || DEFAULT_RPC_URL
 export const AGENT_REGISTRY_ADDRESS =
   process.env.NEXT_PUBLIC_AGENT_REGISTRY_ADDRESS ||
