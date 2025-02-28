9331f35334614054a6ee9fb44241a92c8cd75da9
VEERENDRA VAMSHI <136776337+MKVEERENDRA@users.noreply.github.com>
2025-03-01 04:12:36 +0530

    Update LeaderboardSkeleton.tsx\

--- frontend/components/ui/skeletons/LeaderboardSkeleton.tsx
+++ frontend/components/ui/skeletons/LeaderboardSkeleton.tsx
@@ -1,44 +1,32 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { AgentListView } from "@/components/AgentListView";
-import { TEXT_COPIES } from "@/constants";
-
-export default function LeaderboardPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const activeTabFromUrl = searchParams.get("active") || "attackers";
-  const [activeTab, setActiveTab] = useState(activeTabFromUrl);
-
-  // Update URL when tab changes
-  useEffect(() => {
-    const params = new URLSearchParams(searchParams);
-    params.set("active", activeTab);
-    router.push(`?${params.toString()}`, { scroll: false });
-  }, [activeTab]);
-
+export const LeaderboardSkeleton = () => {
   return (
-    <div className="mt-16 md:mt-0 min-h-screen bg-cover bg-center bg-no-repeat text-white flex-col items-end md:items-center justify-center md:px-4">
-      <div className="flex space-x-4 mb-4">
-        <button
-          className={`px-4 py-2 ${activeTab === "attackers" ? "bg-blue-500" : "bg-gray-700"}`}
-          onClick={() => setActiveTab("attackers")}
-        >
-          Top Attackers
-        </button>
-        <button
-          className={`px-4 py-2 ${activeTab === "defenders" ? "bg-blue-500" : "bg-gray-700"}`}
-          onClick={() => setActiveTab("defenders")}
-        >
-          Top Defenders
-        </button>
+    <div className="text-xs flex flex-col gap-1 whitespace-nowrap overflow-x-auto">
+      <div className="grid grid-cols-12 bg-[#2E40494D] backdrop-blur-xl min-w-[680px] min-h-10 p-4 rounded-lg mb-2 animate-pulse gap-5">
+        <div className="col-span-5 md:col-span-3 grid grid-cols-12 items-center">
+          <div className="pr-1 col-span-2 lg:col-span-1 h-3 bg-gray-700 rounded"></div>
+          <div className="h-full w-[1px] bg-[#6F6F6F]"></div>
+          <div className="col-span-2 h-3 bg-gray-700 rounded"></div>
+        </div>
+        <div className="col-span-2 border-l border-l-[#6F6F6F] ps-4 h-3 bg-gray-700 rounded w-16"></div>
+        <div className="col-span-3 border-l border-l-[#6F6F6F] ps-4 h-3 bg-gray-700 rounded w-16"></div>
+        <div className="col-span-2 border-l border-l-[#6F6F6F] ps-4 h-3 bg-gray-700 rounded w-16"></div>
       </div>
 
-      <AgentListView
-        heading={TEXT_COPIES.leaderboard.heading}
-        subheading={TEXT_COPIES.leaderboard.subheading}
-      />
+      {new Array(10).fill(1).map((_, index) => (
+        <div
+          className="grid grid-cols-12 bg-[#2E40494D] backdrop-blur-xl min-w-[680px] min-h-11 p-4 rounded-lg animate-pulse gap-1"
+          key={`agent-skeleton-${index}`}
+        >
+          <div className="col-span-5 md:col-span-3 grid grid-cols-12 items-center">
+            <div className="pr-1 col-span-2 lg:col-span-1 h-3 bg-gray-700 rounded"></div>
+            <div className="h-full w-[1px] bg-[#6F6F6F]"></div>
+            <div className="col-span-2 h-3 bg-gray-700 rounded"></div>
+          </div>
+          <div className="col-span-2 border-l border-l-[#6F6F6F] ps-4 h-3 bg-gray-700 rounded w-16"></div>
+          <div className="col-span-3 border-l border-l-[#6F6F6F] ps-4 h-3 bg-gray-700 rounded w-16"></div>
+          <div className="col-span-2 border-l border-l-[#6F6F6F] ps-4 h-3 bg-gray-700 rounded w-16"></div>
+        </div>
+      ))}
     </div>
   );
-}
+};
