b96d53bbae489feea99be12bd63d1ef2503bae47
Jorik Schellekens <joriksch@gmail.com>
2025-02-28 08:41:15 +0000

    Remove extension

--- extension/src/config/network.ts
+++
@@ -1,9 +0,0 @@
-export const ACTIVE_NETWORK = {
-  tokens: {
-    ETH: {
-      symbol: 'ETH',
-      decimals: 18,
-      address: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
-    }
-  }
-} 
--- extension/src/content-script/components/starknetkit-connectors.tsx
+++
@@ -1,8 +0,0 @@
-import { InjectedConnector } from "starknetkit/injected";
-import { WebWalletConnector } from "starknetkit/webwallet";
-
-export const availableConnectors = [
-  new InjectedConnector({ options: { id: "argentX" } }),
-  new InjectedConnector({ options: { id: "braavos" } }),
-  new WebWalletConnector({ url: "https://web.argent.xyz" }),
-];
